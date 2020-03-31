a0e800a766bb9a6ca4829eb4bf2b5867609d4760
DutchDS <dutchdatasmarts@gmail.com>
2020-03-31 08:44:16 -0500

    done

--- level-1/after_schema.sql
+++ level-1/after_schema.sql
@@ -15,3 +15,36 @@ CREATE TABLE products (
   product_title TEXT,
   product_category TEXT
 );
+
+-- Create view for easy access to all data used in Query 1
+DROP VIEW IF EXISTS amazon_reviews_all_v CASCADE;
+CREATE VIEW amazon_reviews_all_v AS (
+	SELECT r.review_id, r.review_date, r.product_parent, c.customer_id,  c.customer_count,  p.product_id, p.product_title, p.product_category, v.star_rating, v.helpful_votes, v.total_votes, vine  
+	FROM review_id_table r 
+	JOIN vine_table v on r.review_id = v.review_id
+	JOIN products p on r.product_id = p.product_id
+	JOIN customers c on r.customer_id = c.customer_id
+);
+
+-- Create view to summarize data at product category and vine (Y/N) level used in Query 2
+DROP VIEW IF EXISTS star_rating_V;
+CREATE VIEW star_rating_V AS (
+SELECT product_category, vine,
+	CASE WHEN star_rating = 1 THEN count(review_id) ELSE 0 END as star_1,
+	CASE WHEN star_rating = 2 THEN count(review_id) ELSE 0 END as star_2,
+	CASE WHEN star_rating = 3 THEN count(review_id) ELSE 0 END as star_3,
+	CASE WHEN star_rating = 4 THEN count(review_id) ELSE 0 END as star_4,
+	CASE WHEN star_rating = 5 THEN count(review_id) ELSE 0 END as star_5,
+	count(review_id) as total
+FROM amazon_reviews_all_v
+GROUP BY product_category, star_rating, vine
+
+-- Create view to summarize total and helpful votes and total reviews per vine (Y/N) level used in Query 3	
+CREATE VIEW helpfull_votes_v AS
+SELECT product_category, vine, 
+	sum(helpful_votes) as helpful_votes, 
+	sum(total_votes) as total_votes,
+	count(review_id) as total_reviews
+FROM amazon_reviews_all_v
+GROUP BY product_category, vine
+);
--- level-2/analysis_queries.sql
+++ level-2/analysis_queries.sql
@@ -1,33 +1,49 @@
--- truncate table customers;
--- truncate table products;
--- truncate table review_id_table;
--- truncate table vine_table;
-
-select count(*) from customers;
--- Output Pet Products: 103016
--- Output Musical Instruments: 151018 - 103016 
-select count(*) from products;
--- Output Pet Products: 48050 
--- Output Musical Instruments: 48050 - 37234
-select count(*) from review_id_table;
--- Output Pet Products: 173127
--- Output Musical Instruments: 173127 - 117813
-select count(*) from vine_table;
--- Output Pet Products: 173127 - 117813
--- Output Musical Instruments: 173127 - 117813
-
-CREATE VIEW amazon_reviews_all_v AS (
-	SELECT r.review_id, r.review_date, r.product_parent, c.customer_id,  c.customer_count,  p.product_id, p.product_title, p.product_category, v.star_rating, v.helpful_votes, v.total_votes, vine  
-	FROM review_id_table r 
-	JOIN vine_table v on r.review_id = v.review_id
-	JOIN products p on r.product_id = p.product_id
-	JOIN customers c on r.customer_id = c.customer_id
-)
-
-SELECT product_category, count(*) from amazon_reviews_all_v
-GROUP BY product_category;
-
+-- Query 1 - Total reviews by product category and vine/non-vine reviewer. Output: Q1_output.png
 SELECT product_category, vine, count(*) from amazon_reviews_all_v
 GROUP BY product_category, vine
 
-SELECT * FROM vine_table
+-- Query 2a - Number of review by star rating and vine review (Y/N). Output: Q2a_output.png
+SELECT 
+product_category, vine,
+SUM(star_1) as star_1, SUM(star_2) as star_2, SUM(star_3) as star_3, SUM(star_4) as star_4, SUM(star_5) as star_5, sum(total) as total
+FROM star_rating_V
+GROUP BY product_category, vine;
+
+-- Query 2b - Number of review by star rating and vine review (Y/N) in percentages. Output Q2b_output.png
+SELECT 
+product_category, vine,
+ROUND(SUM(star_1)/SUM(total)*100,2)||'%' as star_1, 
+ROUND(SUM(star_2)/SUM(total)*100,2)||'%' as star_2, 
+ROUND(SUM(star_3)/SUM(total)*100,2)||'%' as star_3, 
+ROUND(SUM(star_4)/SUM(total)*100,2)||'%' as star_4, 
+ROUND(SUM(star_5)/SUM(total)*100,2)||'%' as star_5, 
+ROUND(sum(total)/SUM(total)*100,1) ||'%' as total
+FROM star_rating_V
+GROUP BY product_category, vine
+
+SELECT 
+vine,
+ROUND(SUM(star_1)/SUM(total)*100,2) as star_1, 
+ROUND(SUM(star_2)/SUM(total)*100,2) as star_2, 
+ROUND(SUM(star_3)/SUM(total)*100,2) as star_3, 
+ROUND(SUM(star_4)/SUM(total)*100,2) as star_4, 
+ROUND(SUM(star_5)/SUM(total)*100,2) as star_5, 
+ROUND(sum(total)/SUM(total)*100,1) as total
+FROM star_rating_V
+GROUP BY vine
+
+-- Query 3 How do vine reviews relate to non-vine reviews when it comes to total and helpful votes. Output: Q3_output.png
+SELECT
+	product_category,
+	vine,
+	helpful_votes,
+	total_votes,
+	round(sum(helpful_votes)/sum(total_votes)*100,2)||'%' as pct_helpful,
+	total_votes/total_reviews avg_total_per_review,
+	helpful_votes/total_reviews avg_helpfull_per_review
+FROM helpfull_votes_v
+GROUP BY product_category,
+	vine,
+	helpful_votes,
+	total_votes,
+	total_reviews
